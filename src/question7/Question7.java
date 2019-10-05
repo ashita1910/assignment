acc2da62d320beae3ae56710a94991dedd9bc262
ashitag64 <ashita.goyal_cs18@gla.ac.in>
2019-10-05 23:01:39 +0530

    print the desired output

--- src/question7/Question7.java
+++ src/question7/Question7.java
@@ -1,4 +1,36 @@
 package question7;
 
+import java.util.Scanner;
+
 public class Question7 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String val = scanner.nextLine().trim();
+        Solution7 sol7 = new Solution7(val);
+        System.out.println(sol7.display());
+    }
+}
+
+class Solution7 {
+    private String str;
+
+    Solution7(String str) {
+        this.str = str;
+    }
+
+    public String getStr() {
+        return str;
+    }
+
+    public void setStr(String str) {
+        this.str = str;
+    }
+
+    public int display() {
+        int count = 0;
+        for (int i = 0; i < str.length(); i++) {
+            count += (int) (str.charAt(i));
+        }
+        return count;
+    }
 }
