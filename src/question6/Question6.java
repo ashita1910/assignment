31ab66850f46c8a44337fbd00606791703f005ad
ashitag64 <ashita.goyal_cs18@gla.ac.in>
2019-10-05 22:49:09 +0530

    print the desired output

--- src/question6/Question6.java
+++ src/question6/Question6.java
@@ -1,4 +1,32 @@
 package question6;
 
+import java.util.Scanner;
+
 public class Question6 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String val = scanner.nextLine().trim();
+        Solution6 sol6 = new Solution6(val);
+        System.out.println(sol6.toUpper());
+    }
+}
+
+class Solution6 {
+    private String str;
+
+    Solution6(String str) {
+        this.str = str;
+    }
+
+    public String getStr() {
+        return str;
+    }
+
+    public void setStr(String str) {
+        this.str = str;
+    }
+
+    public String toUpper() {
+        return str.toUpperCase();
+    }
 }
