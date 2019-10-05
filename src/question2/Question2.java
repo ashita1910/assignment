ef2129504c7787b3dd89496fe9da3b5f0580dea1
ashitag64 <ashita.goyal_cs18@gla.ac.in>
2019-10-05 21:47:34 +0530

    print the desired output

--- src/question2/Question2.java
+++ src/question2/Question2.java
@@ -1,4 +1,43 @@
 package question2;
 
+import java.util.Scanner;
+
 public class Question2 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String str = scanner.nextLine().trim();
+        int no = scanner.nextInt();
+        Solution2 sol2 = new Solution2(str, no);
+        System.out.println(sol2.subString());
+    }
+}
+
+class Solution2 {
+    private String name;
+    private int index;
+
+    Solution2(String name, int index) {
+        this.name = name;
+        this.index = index;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getIndex() {
+        return index;
+    }
+
+    public void setIndex(int index) {
+        this.index = index;
+    }
+
+    public String subString() {
+        return name.substring(index);
+    }
 }
